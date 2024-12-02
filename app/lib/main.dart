1f33b0e35c6f0cfee02d98eaf1e36ff04376251e
Axel Wennström <axel.wennstroem@gmail.com>
2024-12-01 22:32:18 -0800

    Restructure repo to fit more things than the app

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,7 +1,5 @@
 import 'dart:async';
-import 'dart:convert';
 import 'dart:io';
-import 'package:flutter/services.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:csv/csv.dart';
 import 'package:flutter/material.dart';
